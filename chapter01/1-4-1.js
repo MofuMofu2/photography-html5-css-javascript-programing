c2d66876fbf0e5ff3ca567b9a2c69089a58478c5
MofuMofu2 <froakie002@gmail.com>
2019-06-08 18:01:55 +0900

    [update] #1 数値のバリデーションチェックを入れてみた

--- chapter01/1-4-1.js
+++ chapter01/1-4-1.js
@@ -2,16 +2,25 @@ const button = document.getElementById("calc");
 
 function calcBMI() {
   // 本ではvarになっているが、再代入が必要でなければconstが推奨されるので変更する
-  const inputWeight = document.getElementById("weight");
-  const inputHeight = document.getElementById("height");
+  const inputWeight = document.getElementById("weight").value;
+  const inputHeight = document.getElementById("height").value;
 
-  // BMI = 身長 / 体重の二乗
-  const bmiResult = inputWeight / (inputHeight * inputHeight);
+  // 入力された値を数値に変換する
+  const convertWeight = parseInt(inputWeight);
+  const convertHeight = parseInt(inputHeight);
 
-  // 結果の表示
-  document.getElementById(
-    "result"
-  ).textContent = `あなたのBMIは${bmiResult}です。`;
+  // 数値だったら計算する
+  if (!isNaN(convertWeight) && !isNaN(convertHeight)) {
+    // BMI = 身長 / 体重の二乗
+    const bmiResult = inputWeight / (inputHeight * inputHeight);
+
+    // 結果の表示
+    document.getElementById(
+      "result"
+    ).textContent = `あなたのBMIは${bmiResult}です。`;
+  } else {
+    document.getElementById("result").textContent = "整数を入力してください。";
+  }
 }
 
 button.addEventListener("click", calcBMI);
