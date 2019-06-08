e2d7d1046a42eb850938fc5df75e333feeeeeaaa
MofuMofu2 <froakie002@gmail.com>
2019-06-08 21:40:34 +0900

    [add] #5 三角形を書いてみる

--- chapter04/4-2-1.js
+++ chapter04/4-2-1.js
@@ -14,3 +14,16 @@ ctx.moveTo(100, 100);
 ctx.lineTo(150, 50);
 ctx.lineTo(200, 100);
 ctx.stroke();
+
+// 別の図形を作る
+ctx.beginPath();
+ctx.strokeStyle = "#ff00ff";
+ctx.moveTo(300, 100);
+// 短い辺を1本描く
+ctx.lineTo(350, 50);
+// 長い辺を1本描く
+ctx.lineTo(400, 100);
+// 辺を閉じる
+ctx.closePath();
+// 描画する
+ctx.stroke();
