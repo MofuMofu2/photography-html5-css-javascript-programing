a5a40dd308839b14f300aa34193fdb3ba1c4e0da
MofuMofu2 <froakie002@gmail.com>
2019-06-08 14:57:25 +0900

    [add] #1 1-4-1の写経
    ファイルは外に切り出してみた。どうやって素のJSでイベント検知するのか？

---
+++ chapter01/1-4-1.js
@@ -0,0 +1,13 @@
+function calcBMI() {
+  // 本ではvarになっているが、再代入が必要でなければconstが推奨されるので変更する
+  const inputWeight = document.getElementById("weight");
+  const inputHeight = document.getElementById("height");
+
+  // BMI = 身長 / 体重の二乗
+  const bmiResult = inputWeight / (inputHeight * inputHeight);
+
+  // 結果の表示
+  document.getElementById(
+    "result"
+  ).textContent = `あなたのBMIは${bmiResult}です。`;
+}
